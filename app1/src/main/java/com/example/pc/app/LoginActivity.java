2956fd6e402e9d73b17d6981c7848833607c364c
brownlf <1842408382@qq.com>
2017-10-17 00:17:01 +0800

    init

---
+++ app1/src/main/java/com/example/pc/app/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.example.pc.app;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by pc on 2017/10/16.
+ */
+
+public class LoginActivity extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
